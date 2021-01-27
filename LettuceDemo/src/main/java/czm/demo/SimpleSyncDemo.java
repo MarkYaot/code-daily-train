e1af7037522196da24549dc076157b626594d275
caizhaomin <c00567567@china.huawei.com>
2021-01-28 00:32:41 +0800

    record some minds

--- LettuceDemo/src/main/java/czm/demo/SimpleSyncDemo.java
+++ LettuceDemo/src/main/java/czm/demo/SimpleSyncDemo.java
@@ -1,8 +1,12 @@
 package czm.demo;
 
 import io.lettuce.core.RedisURI;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import czm.util.PropertiesUtil;
 import io.lettuce.core.RedisClient;
@@ -15,7 +19,7 @@ import io.lettuce.core.api.sync.RedisCommands;
 public class SimpleSyncDemo {
     private static final Logger logger = LoggerFactory.getLogger(SimpleSyncDemo.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String host = PropertiesUtil.getRedisProperties("host");
         String port = PropertiesUtil.getRedisProperties("single.port");
 
@@ -23,8 +27,11 @@ public class SimpleSyncDemo {
         RedisClient client = RedisClient.create(redisURI);
         StatefulRedisConnection<String, String> connection = client.connect();
         RedisCommands<String, String> commands = connection.sync();
-        commands.set("name", "czm");
-        logger.info(commands.get("name"));
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            commands.set(String.valueOf(i), "xxx");
+        }
+        logger.info("cost {} ms", System.currentTimeMillis() - time);
         connection.close();
         client.shutdown();
     }
